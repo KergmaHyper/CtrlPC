c898b739998e261811a556a20051331f287569a9
kergma@gmail.com <kergma@gmail.com>
2021-08-18 17:17:10 +0300

    two

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+import pyautogui as pg
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#pg.move(100, 100, 0.5)
+#pg.drag(200, 100, 0.5, button = 'left')
+#print(pg.position())
+#pg.click(11, 160, duration = 0.5)
+#pg.click(11, 90, duration = 0.5)
+
+#pg.typewrite("\n#hello world\n", 0.1)
+
+#hello world
+#hello world
+
+#pg.moveTo(1041, 75)
+#pg.click(1041, 75)
+#pg.typewrite("google.com")
+#pg.typewrite(["enter"])
+
+#pg.hotkey("winleft")
+#pg.typewrite("chrome\n", 0.5)
+#pg.typewrite("www.youtube.com\n", 0.2)
+#pg.hotkey("winleft", "up")
+#pg.hotkey("ctrl", "t")
+#pg.typewrite("www.sd.ua\n")
+
+#pg.alert("Message", "title", button = "butt text")
+#age = pg.prompt("Enter your age", "your Age")
+#print(f"Your Age: {age}")
+
+#st = pg.confirm("Press any key","choice key",("one","two","three"))
+#print( st )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
